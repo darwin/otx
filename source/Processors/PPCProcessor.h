d692caeb253d47e8cce9757cfd17392a93975fb3
otxdev <otxdev@5968870d-8a1b-0410-8ef2-9208d525af3e>
2007-10-07 23:25:18 +0000

    fixed IS_BLR logic
    git-svn-id: http://otx.osxninja.com/builds/trunk@326 5968870d-8a1b-0410-8ef2-9208d525af3e

--- source/Processors/PPCProcessor.h
+++ source/Processors/PPCProcessor.h
@@ -65,7 +65,7 @@
 	(((PO((i)) == 0x13 && SO((i)) == 0x10) || (PO((i)) == 0x10))	\
 	&& (BO((i)) != 0x14))
 
-#define IS_BLR(i)	(PO((i)) == 0x13)
+#define IS_BLR(i)	((i) == 0x4e800020)
 
 // Addresses and names of functions in the ObjC runtime page, stolen from
 // http://www.opensource.apple.com/darwinsource/10.4.7.ppc/objc4-267.1/runtime/objc-rtp.h
